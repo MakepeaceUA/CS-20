08153174ca7820c88d4568ba5a0e8a19d6d57c3c
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-10-11 19:35:27 +0300

    Add files via upload

---
+++ Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Article
+    {
+        public string Title { get; set; }
+        public int CharCount { get; set; }
+        public string Summary { get; set; }
+
+    }
+}
---
+++ Journal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp12
+{
+    internal class Journal
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public DateTime Date { get; set; }
+        public int PageCount { get; set; }
+        public List<Article> Articles { get; set; } = new List<Article>();
+
+        public void Output()
+        {
+            Console.WriteLine($"Название журнала: {Title},Издательство: {Publisher},Дата издания: {Date.ToShortDateString()},Количество страниц: {PageCount} \n\nСтатьи:");
+            foreach (var a in Articles)
+            {
+                Console.WriteLine($"Название: {a.Title},Количество символов: {a.CharCount},Анонс: {a.Summary}");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
